195fa125c5f4ce6f84cb519035708d1bbffaf220
Remez19 <rdoneplus@gmail.com>
2022-05-18 16:08:18 +0300

    Lecture_134_COMPLETE

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MealImg from "../../Assets/food.jpg";
 import styles from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={styles.header}>
         <h1>Remez's Cuisine</h1>
+        <HeaderCartButton />
       </header>
       <div className={styles.main_image}>
         <img src={MealImg} alt="Delicious Food!"></img>
